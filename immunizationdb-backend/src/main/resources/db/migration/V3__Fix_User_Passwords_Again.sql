e6c7ea75ade6213b371cb5e5b8c604d674ad48d0
donjunior01 <juniorassobjio@.com>
2025-12-05 06:35:34 +0100

    deleted clean and exploitable adjustment for the better code functioning

--- immunizationdb-backend/src/main/resources/db/migration/V3__Fix_User_Passwords_Again.sql
+++
@@ -1,5 +0,0 @@
--- Fix user passwords with properly formatted BCrypt hash for "Password123!"
--- Using dollar-quote syntax to avoid escaping issues
-UPDATE users 
-SET password = '$2a$12$.4qFisHXCJZsQog8d8iYveyZJg7dVasFEy1/zJzShHvvkDubdpwqi'
-WHERE username IN ('health.worker', 'facility.manager', 'gov.official');
